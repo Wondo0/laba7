68d63f240696d3be079e56af7c6d7250de447474
Wondo0 <borodinoleksandrr@gmail.com>
2026-04-06 16:59:21 +0300

    Initial

---
+++ Lab7/Program.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+
+namespace Lab7Node;
+
+public class Node
+{
+    public short Data { get; set; }
+
+    public Node? Next { get; set; }
+
+    public Node(short data)
+    {
+        ArgumentNullException.ThrowIfNull(data);
+        this.Data = data;
+        this.Next = null;
+    }
+}
+
+public class NodeList : IEnumerable<short>
+{
+    private Node? head;
+    private Node? tail;
+
+    public void addNext(short data)
+    {
+        Node newNode = new Node(data);
+
+        if (this.head == null)
+        {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else
+        {
+            this.tail.Next = newNode;
+            this.tail = newNode;
+        }
+    }
+
+    public short? findMultipleBy(short dividedBy)
+    {
+        if (dividedBy == 0)
+        {
+            throw new ArgumentException("Ділення на нуль неможливе.");
+        }
+
+        Node? current = this.head;
+
+        while (current != null)
+        {
+            if (current.Data % dividedBy == 0)
+            {
+                return current.Data;
+            }
+            current = current.Next;
+        }
+
+        return null;
+    }
+
+    public void replaceEvenNodesToArgument(short argument = 0)
+    {
+        Node? current = this.head;
+        int position = 0;
+        while (current != null)
+        {
+            if (position % 2 == 0)
+            {
+                current.Data = argument;
+            }
+            current = current.Next;
+            position++;
+        }
+    }
+
+    public NodeList? getNewListWithElementsGreaterThan(short data)
+    {
+        Node? current = this.head;
+        NodeList newList = new NodeList();
+        while (current != null)
+        {
+            if (current.Data > data)
+            {
+                newList.addNext(data);
+            }
+            current = current.Next;
+        }
+
+        return newList;
+    }
+
+    public void deleteOddNodes()
+    {
+        Node? current = this.head;
+
+        while (current != null && current.Next != null)
+        {
+            Node nodeToRemove = current.Next;
+            
+            current.Next = nodeToRemove.Next;
+
+            if (current.Next == null)
+            {
+                this.tail = current;
+            }
+
+            current = current.Next;
+        }
+    }
+
+    public short this[int index]
+    {
+        get
+        {
+            if (index < 0)
+            {
+                throw new ArgumentException("Індекс не може бути від'ємним");
+            }
+
+            Node? current = head;
+            int currentIndex = 0;
+
+            while (current != null)
+            {
+                if (currentIndex == index)
+                {
+                    return current.Data;
+                }
+                current = current.Next;
+                currentIndex++;
+            }
+
+            throw new IndexOutOfRangeException("Індекс більше ніж розмір списку");
+        }
+    }
+
+    public void RemoveElement(int index)
+    {
+        if (index < 0 || head == null)
+            throw new IndexOutOfRangeException("Індекс виходить за межі списку або список порожній.");
+
+        if (index == 0)
+        {
+            head = head.Next;
+            if (head == null)
+                tail = null;
+            return;
+        }
+
+        Node current = head;
+        int currentIndex = 0;
+
+        while (current != null && currentIndex < index - 1)
+        {
+            current = current.Next;
+            currentIndex++;
+        }
+
+        if (current == null || current.Next == null)
+            throw new IndexOutOfRangeException("Індекс виходить за межі списку.");
+
+        current.Next = current.Next.Next;
+
+        if (current.Next == null)
+        {
+            tail = current;
+        }
+    }
+
+    public IEnumerator<short> GetEnumerator()
+    {
+        Node? current = head;
+        while (current != null)
+        {
+            yield return current.Data;
+            current = current.Next;
+        }
+    }
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ Lab7Use/Program.cs
@@ -0,0 +1,168 @@
+﻿using Lab7Node;
+
+public class UsageOfNodes
+{
+    static void Main()
+    {
+        try
+        {
+            NodeList list = new NodeList();
+
+            Console.WriteLine("Введіть початковий масив даних для запису у список через пробіл");
+            string[] initialDataStringArray = Console.ReadLine().Split(" ");
+
+            short[] initialDataArray = new short[initialDataStringArray.Length];
+            int count = 0;
+
+            for (int i = 0; i < initialDataStringArray.Length; i++)
+            {
+                if (short.TryParse(initialDataStringArray[i], out short temp))
+                {
+                    initialDataArray[count] = temp;
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine($"{initialDataStringArray[i]} не є типом даних short і не був доданий до списку");
+                }
+            }
+
+            Array.Resize(ref initialDataArray, count);
+            foreach(short item in initialDataArray)
+            {
+                list.addNext(item);
+            }
+            Console.WriteLine("Ваш початковий список:" + string.Join(", ", initialDataArray));
+
+            Console.WriteLine("Введіть номер бажаної процедури\n 1 - знаходження елементу\n 2 - Пошук кратних елеметів\n 3 - Заміна елементів індекс яких є парним числом\n 4 - видалення елементів на непарних позиціях\n 5 - Додавання елементу у кінець списку\n 6 - Видалення елементу по індексу");
+            if (int.TryParse(Console.ReadLine(), out int idOfProcedure) && idOfProcedure < 7 && idOfProcedure > 0)
+            {
+            
+            }
+            else
+            {
+                Console.WriteLine("Некоректний ввід номеру процедури, номер повинен бути цілочисельним числом в межах [1;4]");
+            }
+
+            switch (idOfProcedure)
+            {
+                case 1:
+                    findIndex(list);
+                    break;
+                case 2:
+                    findDivisors(list);
+                    break;
+                case 3:
+                    ReplaceEvenElements(list);
+                    break;
+                case 4:
+                    RemovingOddElements(list);
+                    break;
+                case 5:
+                    AddElementToTheEnd(list);
+                    break;
+                case 6:
+                    RemoveElement(list);
+                    break;
+            }
+        } catch (Exception ex)
+        {
+            Console.WriteLine("Помилка. Текст помилки:" + ex.Message);
+        }
+    }
+
+    public static void findIndex(NodeList list)
+    {
+        Console.WriteLine("Введіть індекс для знаходження елементу");
+        if (int.TryParse(Console.ReadLine(), out int index))
+        {
+            Console.WriteLine(list[index]);
+        }
+        else
+        {
+            Console.WriteLine("Некоректний ввід індексу, індекс повинен бути цілочисельним числом");
+        }
+    }
+
+    public static void findDivisors(NodeList list)
+    {
+        Console.WriteLine("Введіть дільник для пошуку чисел кратних йому");
+        if (short.TryParse(Console.ReadLine(), out short divisor))
+        {
+            short? multiple = list.findMultipleBy(divisor);
+            if (multiple != null)
+            {
+                Console.WriteLine($"Перший елемент, кратний {divisor} - {multiple.Value}");
+            }
+            else
+            {
+                Console.WriteLine($"Елемент кратний {divisor} не знайдено.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Введене некоректне значення дільника");
+        }
+    }
+
+    public static void ReplaceEvenElements(NodeList list)
+    {
+        Console.WriteLine("Введіть число для заміни елементів індекс яких є парним числом, за замовченням нуль (початковий список не буде змінюватися)");
+
+        string replaceArgument = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(replaceArgument))
+        {
+            list.replaceEvenNodesToArgument();
+        }
+        else
+        {
+            if (short.TryParse(replaceArgument, out short parsedReplaceArgument))
+            {
+                list.replaceEvenNodesToArgument(parsedReplaceArgument);
+                Console.WriteLine("Ваш список із заміною елементів з парних індексом на інше значення:" + string.Join(", ", list));
+            } else
+            {
+                Console.WriteLine("Значення не є в межах значень типу даних short");
+            }
+        }
+    }
+
+    public static void RemovingOddElements(NodeList list)
+    {
+        Console.WriteLine("Автоматичне видалення елементів на непарних позиціях на копії списку");
+        list.deleteOddNodes();
+        Console.WriteLine("Ваш список з видаленням елементів з непарними індексами:" + string.Join(", ", list));
+    }
+
+    public static void AddElementToTheEnd(NodeList list)
+    {
+        Console.WriteLine("Введіть значення елементу для додавання у кінець");
+        if (short.TryParse(Console.ReadLine(), out short data))
+        {
+            Console.WriteLine("Ваш список до додавання:" + string.Join(", ", list));
+            list.addNext(data);
+            Console.WriteLine("Ваш список після додавання:" + string.Join(", ", list));
+
+        }
+        else
+        {
+            Console.WriteLine("Некоректне значення елементу, елемент має бути в межах значень типу даних short");
+        }
+    }
+
+    public static void RemoveElement(NodeList list)
+    {
+        Console.WriteLine("Введіть індекс елементу для видалення");
+        if (short.TryParse(Console.ReadLine(), out short index))
+        {
+            Console.WriteLine($"Ваш список до видалленя елементу з індексом {index}:" + string.Join(", ", list));
+            list.RemoveElement(index);
+            Console.WriteLine($"Ваш список після видаленняелементу з індексом {index}:" + string.Join(", ", list));
+
+        }
+        else
+        {
+            Console.WriteLine("Некоректне значення елементу, елемент має бути в межах значень типу даних short");
+        }
+    }
+}
